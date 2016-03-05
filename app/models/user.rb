36e44a9fff7d86f2b2577e9b06373a7b2ac6b1ac
JD Crouch <jdcrouch5@gmail.com>
2016-03-05 13:19:18 +0900

    Add delete action to list and user controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: { minimum: 4 }
   validates :password, presence: true, length: { minimum: 6 }
 
-  has_many :lists
+  has_many :lists, dependent: :destroy
 end
